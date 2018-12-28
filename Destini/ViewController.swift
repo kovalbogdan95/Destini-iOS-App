7fda808ec4e7ef06f8de0f7e0e778718305eee8a
Bogdan Kovalenko <kovalbogdan95@redwerk.com>
2018-12-29 00:40:54 +0200

    Finish Destini app

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -28,6 +28,8 @@ class ViewController: UIViewController {
     let story5 = "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in."
     let story6 = "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\""
     
+    var currentStory = 1
+    let endButtonText = "Game over"
     
     // UI Elements linked to the storyboard
     @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
@@ -37,11 +39,21 @@ class ViewController: UIViewController {
     // TODO Step 5: Initialise instance variables here
     
     
-    
+    func setStoryView(storyText: String, firstAnswer: String, secondAnswer: String) {
+        storyTextView.text = storyText
+        if (firstAnswer == endButtonText) {
+            bottomButton.isHidden = true
+            topButton.setTitle(firstAnswer, for: .normal)
+        } else {
+            bottomButton.isHidden = false
+            topButton.setTitle(firstAnswer, for: .normal)
+            bottomButton.setTitle(secondAnswer, for: .normal)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setStoryView(storyText: story1, firstAnswer: answer1a, secondAnswer: answer1b)
         
         // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
         
@@ -51,6 +63,33 @@ class ViewController: UIViewController {
     // User presses one of the buttons
     @IBAction func buttonPressed(_ sender: UIButton) {
     
+        if ((currentStory == 1 && sender.tag == 1) || (currentStory == 2 && sender.tag == 1)) {
+            setStoryView(storyText: story3, firstAnswer: answer3a, secondAnswer: answer3b)
+            currentStory = 3
+        } else if (currentStory == 1 && sender.tag == 2) {
+            setStoryView(storyText: story2, firstAnswer: answer2a, secondAnswer: answer2b)
+            currentStory = 2
+        } else if (currentStory == 2 && sender.tag == 2) {
+            setStoryView(storyText: story4, firstAnswer: endButtonText, secondAnswer: "")
+            currentStory = 0
+        } else if (currentStory == 3 && sender.tag == 1){
+            setStoryView(storyText: story6, firstAnswer: endButtonText, secondAnswer: "")
+            currentStory = 0
+        } else if (currentStory == 3 && sender.tag == 2){
+            setStoryView(storyText: story5, firstAnswer: endButtonText, secondAnswer: "")
+            currentStory = 0
+        } else if (currentStory == 0) {
+            let alert = UIAlertController(title: "Awesome", message: "You've finished all the questions, do you want to start over?", preferredStyle: .alert)
+            
+            let restartAction = UIAlertAction(title: "Restart?", style: .default) { (UIAlertAction) in
+                self.setStoryView(storyText: self.story1, firstAnswer: self.answer1a, secondAnswer: self.answer1b)
+                self.currentStory = 1
+            }
+            
+            alert.addAction(restartAction)
+            
+            present(alert, animated: false) {}
+        }
         // TODO Step 4: Write an IF-Statement to update the views
                 
         // TODO Step 6: Modify the IF-Statement to complete the story
